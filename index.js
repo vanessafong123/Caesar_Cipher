406b0afaaff65b19950892c1e36d2dae375b599a
github_vanessafong123 <fong_vanessa@hotmail.com>
2023-09-25 17:58:19 +0100

    adding index.js

---
+++ index.js
@@ -0,0 +1,46 @@
+const btnEncrypt = document.querySelector('#encrypt');
+const btnDecrypt = document.querySelector('#decrypt');
+const keyInput = document.querySelector('#key');
+const textarea = document.querySelector('#text');
+
+const listLetters = 'abcdefghijklmnopqrstuvwxyz';
+
+function caesarCipherEncrypt(plaintext, shift) {
+  return plaintext
+    .split('')
+    .map((char) => {
+      if (!listLetters.includes(char)) {
+        return char;
+      }
+
+      const isUpperCase = char === char.toUpperCase();
+      const charIndex = listLetters.indexOf(char.toLowerCase());
+      let newIndex = (charIndex + shift) % 26;
+
+      if (newIndex < 0) {
+        newIndex += 26;
+      }
+
+      if (isUpperCase) {
+        return listLetters[newIndex].toUpperCase();
+      }
+      return listLetters[newIndex];
+    })
+    .join('');
+}
+
+function caesarCipherDecrypt(ciphertext, shift) {
+  return caesarCipherEncrypt(ciphertext, -shift);
+}
+
+btnEncrypt.addEventListener('click', () => {
+  const shiftValue = parseInt(keyInput.value, 10);
+  const encryptedText = caesarCipherEncrypt(textarea.value, shiftValue);
+  textarea.value = encryptedText;
+});
+
+btnDecrypt.addEventListener('click', () => {
+  const shiftValue = parseInt(keyInput.value, 10);
+  const decryptedText = caesarCipherDecrypt(textarea.value, shiftValue);
+  textarea.value = decryptedText;
+});
